3e7189b7ae12969a0a4b2370ffc3b87a1bd8db68
Juan Maiz <juanmaiz@gmail.com>
2011-06-19 17:09:02 -0300

    now it works...

--- lib/status_quo/status_quo.rb
+++ lib/status_quo/status_quo.rb
@@ -23,7 +23,7 @@ module StatusQuo
             self.status == '#{status}'
           end
           def #{status}!
-            (status = '#{status}') and save
+            (self.status = '#{status}') and save
           end
         )
       end
@@ -31,3 +31,27 @@ module StatusQuo
   end
 end
 ActiveRecord::Base.send(:include, StatusQuo) if defined?(ActiveRecord)
+
+# IDEAS
+# def self.status_validations &block
+#   validate :status_validations
+#   define_method(:status_validations) do
+#     block.call
+#   end
+# end
+# status_validations do |from, to|
+#   # You can cancel anytime!
+#   return if to == 'cancelled'
+#   case [from, to]
+#   when [:started, :checkout]
+#     errors.add :status, 'Endereço não especificado' unless address
+#     errors.add :status, 'Você precisa comprar ao menos um produto.' if total.to_f <= 0.0
+#   when [:checkout, :payment]
+#   when [:payment, :finished]
+#     errors.add :status, 'Pagamento não especificado' unless payment_method
+#     errors.add :status, 'Cartão não especificado' if payment_method == 'Cartão de crédito' and ! card_id
+#     errors.add :status, 'Cartão não especificado' if payment_method == 'Compra com 1-click' and ! card_id
+#   else
+#     errors.add :status, 'Mudança de status inválida'
+#   end
+# end
